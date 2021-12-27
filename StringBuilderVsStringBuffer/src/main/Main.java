ef6868ad91123b963eafbc6e742dcf6b2da91f5c
LouisPham <tupta1999@gmail.com>
2021-12-27 11:36:59 +0700

    difference between StringBuilder and StringBuffer

--- StringBuilderVsStringBuffer/src/main/Main.java
+++ StringBuilderVsStringBuffer/src/main/Main.java
@@ -12,63 +12,26 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		long startTime = System.currentTimeMillis();
-//		
-//		StringBuffer sb = new StringBuffer("Java");// StringBuffer is synchronize
-//		for(int i = 0; i< 10000; i++) {
-//			sb.append("Hello");
-//		}
-//		System.out.println("The time of StringBuffer concating is: " + (System.currentTimeMillis() - startTime) + " ms");
-//		// Output: 2 ms
-//		
-//		
-//		startTime = System.currentTimeMillis();
-//		
-//		StringBuilder sb2 = new StringBuilder("Java");// StringBuilder is asynchronize, so it better than on performance
-//		for(int i = 0; i< 10000; i++) {
-//			sb.append("Hello");
-//		}
-//		System.out.println("The time of StringBuilder concating is: " + (System.currentTimeMillis() - startTime) + " ms");
-//		// Output 1 ms
+		long startTime = System.currentTimeMillis();
 
-		List<String> arrayList = new ArrayList<String>();
-		arrayList.add("Java");
-		arrayList.add("Angular");
-		arrayList.add("ReactJS");
-		arrayList.add("JavaScript");
-		System.out.println("All elements of ArrayList: ");
-		System.out.println("\t" + arrayList + "\n");
+		StringBuffer sb = new StringBuffer("Java");// StringBuffer is synchronize
+		for (int i = 0; i < 10000; i++) {
+			sb.append("Hello");
+		}
+		System.out
+				.println("The time of StringBuffer concating is: " + (System.currentTimeMillis() - startTime) + " ms");
+		// Output: 2 ms
 
-		List<String> linkedList = new LinkedList<String>();
-		linkedList.add("Java");
-		linkedList.add("Angular");
-		linkedList.add("ReactJS");
-		linkedList.add("JavaScript");
-		System.out.println("All elements of LinkedList: ");
-		System.out.println("\t" + linkedList + "\n");
+		startTime = System.currentTimeMillis();
 
-		// new TreeSet() will order elements
-		// Set does not duplicate
-		Set<String> hashSet = new HashSet<String>();
-		hashSet.add("Java");
-		hashSet.add("Java");
-		hashSet.add("Angular");
-		hashSet.add("ReactJS");
-		hashSet.add("JavaScript");
-		System.out.println("All elements of HashSet: ");
-		System.out.println("\t" + hashSet + "\n");
+		StringBuilder sb2 = new StringBuilder("Java");// StringBuilder is asynchronize, so it better than on performance
+		for (int i = 0; i < 10000; i++) {
+			sb.append("Hello");
+		}
+		System.out
+				.println("The time of StringBuilder concating is: " + (System.currentTimeMillis() - startTime) + " ms");
+		// Output 1 ms
 
-		// new TreeMet() will order elements depend on their key
-		// Map does not duplicate
-		// Map has a pair of key and value
-		Map<String, String> hashMap = new HashMap<String, String>();
-		hashMap.put("Windows", "2000");
-        hashMap.put("Windows", "XP");
-        hashMap.put("Language2", "Java");
-        hashMap.put("Language1", ".Net");
-        System.out.println("All elements of HashMap:");
-        System.out.print("\t" + hashMap);
-// git stash
 	}
 
 }
